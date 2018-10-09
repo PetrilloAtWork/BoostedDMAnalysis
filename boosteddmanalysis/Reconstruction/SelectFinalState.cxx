66d0a85c51be43a171c595855d88a508623fc62b
Yun-Tse Tsai <yuntse@slac.stanford.edu>
2018-10-09 16:54:49 -0700

    add the algorithm to select the stable final state particles

--- boosteddmanalysis/Reconstruction/SelectFinalState.cxx
+++ boosteddmanalysis/Reconstruction/SelectFinalState.cxx
@@ -13,6 +13,7 @@
 // C++ standard library
 #include <iterator> // std::back_inserter()
 #include <memory> // std::addressof()
+#include <map>
 
 
 
@@ -24,19 +25,47 @@ bdm::SelectFinalState::Selection_t bdm::SelectFinalState::select
   
   Selection_t selected; // indices of the selected particles
   
+  std::map<int, std::size_t> ParticleIndices; // ( particle ID , particle index)
+  for (std::size_t index = 0; index < particles.size(); ++index)
+    ParticleIndices[particles[index].TrackId()] = index;
+  
+  std::set<std::size_t> InterestingParticles;
+  
   auto const nParticles = particles.size();
   for (std::size_t index = 0U; index < nParticles; ++index) {
     auto const& particle = particles[index];
     
-    // TODO
+    bool isInteresting = (InterestingParticles.count(index) > 0);
     
-    // filter neutrons out
-    if (!fKeepNeutrons && (std::abs(particle.PdgCode()) == kAbsNeutron))
-      continue;
+    if (particle.Mother() == 0) isInteresting = true;
     
-    // exclude the particles that are not "primary" (i.e. not generated)
-    if (particle.Mother() > 0) continue;
+    if(!isInteresting) continue;
     
+    switch ( std::abs( particle.PdgCode() ) ) {
+      case 311:  // K_0
+      case 130:  // K_L
+      case 310:  // K_S
+      case 111:  // pi0
+        // I am interested in this particle's daughter!
+        for ( int iDaughter = 0; iDaughter < particle.NumberDaughters(); ++iDaughter ) {
+          // find the index of that daughter in the list
+          auto daughterTrackId = particle.Daughter(iDaughter);
+          InterestingParticles.insert(ParticleIndices.at(daughterTrackId));
+        }
+        continue; // not interested in this particle
+      case kAbsNeutron: // neutrons
+        if (!fKeepNeutrons) continue;
+        break;
+      case 2212:  // proton
+      case 211:   // pi+/-
+      case 13:    // muon+/-
+      case 11:    // e+/-
+      case 22:    // photon
+        break; // go out and keep it
+      default:
+        continue;
+    } // switch
+        
     selected.push_back(index);
     
   } // for
